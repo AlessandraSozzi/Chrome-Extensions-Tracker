06de33c34af054b09efde2eae8ba37d5a690db7a
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2016-11-12 22:00:10 +0000

    first implementation with Selenium

--- scraper/scraper/spiders/webstoreScraper.py
+++ scraper/scraper/spiders/webstoreScraper.py
@@ -1,13 +1,42 @@
 # -*- coding: utf-8 -*-
-import scrapy
+
+import re
+import numpy as np
+from selenium import webdriver
+
+from scrapy.http import Request
+from scrapy.spiders import Spider
+from scraper.items import ScraperItem
+from scrapy.selector import HtmlXPathSelector
 
 
-class WebstorescraperSpider(scrapy.Spider):
+class WebstorescraperSpider(Spider):
     name = "webstoreScraper"
     allowed_domains = ["chrome.google.com"]
     start_urls = (
         'https://chrome.google.com/webstore/category/ext/28-photos',
     )
 
+    def __init__(self, *args, **kwargs):
+        self.driver = webdriver.Chrome('/Users/Alessandra/Documents/WebDriver/chromedriver')
+        super(webstoreScraper, self).__init__(*args, **kwargs)
+
     def parse(self, response):
-        pass
+    	
+    	self.driver.get(response.url)
+
+    	time.sleep(10)
+
+    	# Scroll page until end
+    	lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight); return document.body.scrollHeight;")           
+		
+		while True:
+		    lastCount = lenOfPage
+		    time.sleep(5)
+		    lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight); return document.body.scrollHeight;")
+		    if (lastCount == lenOfPage):
+		        break
+
+		
+
+
