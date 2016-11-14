83d838945825660d7f1502c5e02e66fd20094aa7
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2016-11-14 13:48:57 +0000

    scrape groups such as featured items

--- scraper/scraper/spiders/webstoreScraper.py
+++ scraper/scraper/spiders/webstoreScraper.py
@@ -15,11 +15,16 @@ from scrapy.selector import HtmlXPathSelector
 class WebstorescraperSpider(Spider):
     name = "webstoreScraper"
     allowed_domains = ["chrome.google.com"]
-    start_urls = ["https://chrome.google.com/webstore/category/ext/28-photos"]
+    # start_urls = ["https://chrome.google.com/webstore/category/ext/28-photos"]
+    start_urls = ["https://chrome.google.com/webstore/category/extensions"]
 
-    def __init__(self, *args, **kwargs):
-        self.driver = webdriver.Chrome("/Users/Alessandra/Documents/WebDriver/chromedriver")
+    def __init__(self, category = None, *args, **kwargs):
+        # self.driver = webdriver.Chrome("/Users/Alessandra/Documents/WebDriver/chromedriver")
         super(WebstorescraperSpider, self).__init__(*args, **kwargs)
+        if category:
+            self.category = category
+        else:
+            self.category = "Photos"
 
 
     def parse_item(self, response):
@@ -44,8 +49,6 @@ class WebstorescraperSpider(Spider):
     	m = re.search("Average rating\s(.+?)\s", avg_rating)
     	if m:
     		item["avg_rating"] = float(m.group(1))
-    	else:
-    		item["avg_rating"] = np.nan
 
     	# Number of Raters
     	item["raters"] = int(re.sub("\(|\)","", rating.text))
@@ -60,42 +63,98 @@ class WebstorescraperSpider(Spider):
     	# Version
     	item["version"] = item_scope.find('span', attrs = {'class':'C-b-p-D-Xe h-C-b-p-D-md'}).text
 
+        # Group
+        item["group"] = response.meta.get('group', None)
+
+        # Rank
+        item["rank"] = response.meta.get('rank', None)
+
+        # Short and long description
+        description = item_scope.find('div', attrs = {'class':'C-b-p-j-D Ka-Ia-j C-b-p-j-D-gi'})
+        if description:
+            short_text = description.find('div', attrs = {'itemprop':'description'})
+            long_text = description.find('pre')
+        if short_text:
+            item["short_text"] = short_text.text
+        if long_text:
+            item["long_text"] = long_text.text
+
 
     	return item
 
-    	
+    
+    def parse_group(self, response):
+        driver = webdriver.Chrome("/Users/Alessandra/Documents/WebDriver/chromedriver")
+        
+        driver.get(response.url)
+
+        time.sleep(10)
+
+        self.log("I'm in the current category page: %s with group name: %s" % (driver.current_url, response.meta.get('group', None)))
+
+        # Scroll page until end
+        lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight); return document.body.scrollHeight;")           
+
+        while True:
+            lastCount = lenOfPage
+            self.log('lenOfPage %d' % lenOfPage)
+            time.sleep(5)
+            lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight); return document.body.scrollHeight;")
+            if (lastCount == lenOfPage):
+                break
+
+
+        # Extract extensions URLs and yield new Requests with parse_item callback
+        extensions = driver.find_elements_by_xpath("//a[@class='a-u']")
+        self.log("Extensions extracted %d" % len(extensions))
+
+        for rank, extension in enumerate(extensions):
+            url = extension.get_attribute("href")
+            request = Request(url = url, callback = self.parse_item)
+            request.meta.update(rank = rank)
+            request.meta.update(group = response.meta.get('group', None))
+            yield request
+
+        driver.close()
 
 
     def parse(self, response):
-    	
-    	self.driver.get(response.url)
+    	driver = webdriver.Chrome("/Users/Alessandra/Documents/WebDriver/chromedriver")
+    	driver.get(response.url)
 
     	time.sleep(10)
 
-    	# Scroll page until end
-    	lenOfPage = self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight); return document.body.scrollHeight;")           
+        # Click menu
+        menu = driver.find_element_by_xpath("//span[@class='h-n-j-Z-ea-aa ga-dd-Va g-aa-ca']")
+        menu.click()
 
-    	while True:
-    		lastCount = lenOfPage
-    		self.log('lenOfPage %d' % lenOfPage)
-    		time.sleep(5)
-    		lenOfPage = self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight); return document.body.scrollHeight;")
-    		if (lastCount == lenOfPage):
-    			break
+        # Find category
+        options = driver.find_element_by_xpath("//div[@role='listbox']").find_elements_by_xpath("//div[@role='option']")
 
-    	# Extract extensions URLs and yield new Requests with parse_item callback
-    	extensions = self.driver.find_elements_by_xpath("//a[@class='a-u']")
-    	self.log("Extensions extracted %d" % len(extensions))
+        for option in options:
+            option_id = option.get_attribute("id")
+            option_text = option.text
+            self.log("Option found: %s with id %s" % (option_text, option_id))
+            if(option_text == self.category): 
+                driver.find_element_by_xpath("//div[@role='listbox']").find_element_by_xpath("//div[@id='" + option_id + "']").click()
+                time.sleep(10)
+                current_url = driver.current_url
+                break
+        
+        time.sleep(10)
+        
+        
+        # Find extensions groups
+        groups = driver.find_elements_by_xpath("//a[@class='a-K-o-y a-d-zc a-hn-K-o']")
+        groups = [(group.get_attribute("href"), group.find_element_by_xpath("//div[@class='a-K-o-w']").text) for group in groups]
+        driver.close()
+        
+        for (group, group_name) in groups:
+            request = Request(url = group, callback = self.parse_group)
+            request.meta.update(group = group_name)
+            yield request
 
-    	for extension in extensions:
-    		url = extension.get_attribute("href")
-    		yield Request(url = url, callback = self.parse_item)
-
-    	# Second call
-    	yield Request(url = "https://chrome.google.com/webstore/category/collection/top_picks_photos", callback = self.parse)
-
-    def closed(self):
-    	self.driver.close()
+        yield Request(url = current_url, callback = self.parse_group)
 
 
 
